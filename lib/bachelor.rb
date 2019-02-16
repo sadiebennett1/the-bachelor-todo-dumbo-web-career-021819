c3c535ec2c5c37dab5027c9b716a7ce4cc3d6359
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-16 00:36:07 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,20 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  data.each do |seasun, contestants|
+    if seasun == season
+      contestants.each do |hash|
+        if hash.values.include?("Winner")
+          hash.each do |title, descript|
+            if title == "name"
+              arr = descript.split(" ")
+              return arr[0]
+            end
+          end
+        end
+      end
+    end
+  end
+
 end
 
 def get_contestant_name(data, occupation)
